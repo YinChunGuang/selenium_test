5767d1d67e5c5c64f4042742291ae8417bbf16fd
matiji <2549594681@qq.com>
2021-06-17 20:19:07 +0800

    init project

---
+++ seleniumtest/src/main/java/com/mao/BaseModel.java
@@ -0,0 +1,32 @@
+package com.mao;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseModel {
+
+    public static ChromeDriver driver;
+
+    public static void main(String[] args) {
+        initChromeDriver();
+        driver.get("http://localhost:9528");
+        WebElement source = driver.findElementByXPath("//*[@id=\"nodes\"]/li[1]");
+        WebElement target = driver.findElementByXPath("//*[@id=\"canvas_1\"]");
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(source,target);
+
+        driver.quit();
+    }
+
+    private static void initChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", "D:\\java\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+    }
+}
